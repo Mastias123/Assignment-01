6160781892fb5ac1a6809a34706351abf5b027f6
memr <memr@itu.dk>
2022-09-09 13:39:30 +0200

    addObjFiles

--- assignment-01/Assignment1/Iterators.cs
+++ assignment-01/Assignment1/Iterators.cs
@@ -2,6 +2,10 @@ namespace Assignment1;
 
 public static class Iterators
 {
+    public static void Main(String[] args)
+    {
+
+    }
     public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
 
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
