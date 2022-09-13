7f8fceab1ff385857a0469a1b386ce8667b159ee
memr <memr@itu.dk>
2022-09-13 12:34:47 +0200

    IteratorsComplete

--- assignment-01/Assignment1/Iterators.cs
+++ assignment-01/Assignment1/Iterators.cs
@@ -2,7 +2,27 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
-
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+    {
+        foreach (IEnumerable<T> item  in items)
+        {
+            foreach(var i in item)
+            {
+                yield return i;
+            }
+        }
+    }
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+    { 
+        //List<T> filterList = new List<T>();
+        foreach (T item  in items)
+        {
+            if(predicate.Invoke(item))
+            {
+                //filterList.Add(item);
+                yield return item;
+            }
+        }
+        //return filterList.AsEnumerable<T>();
+    }
 }
