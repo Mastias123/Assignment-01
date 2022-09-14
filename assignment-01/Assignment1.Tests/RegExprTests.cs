8bd0f320eefbdb93472d865cc98d3a7c215655ca
memr <memr@itu.dk>
2022-09-14 19:42:42 +0200

    regex1Solved

--- assignment-01/Assignment1.Tests/RegExprTests.cs
+++ assignment-01/Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,33 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void splitLineOnSpaceAndLineShift()
+    {
+        // Arrange
+        List<string> input = new List<string>(){"This is test\nThis is a new line"};
+        List<string> answer = new List<string>{"This", "is", "test", "This", "is", "a", "new", "line"};
+        // Act
+        var splitLine = RegExpr.SplitLine(input);
+        
+        // Assert
+        Assert.Equal(answer, splitLine);
+        
+    }   
+
+    [Fact]
+    public void splitOnResolutions()
+    {
+        // Arrange
+        List<string> input = new List<string>(){"This is test\nThis is a new line"};
+        List<string> answer = new List<string>{"This", "is", "test", "This", "is", "a", "new", "line"};
+        // Act
+        var splitLine = RegExpr.SplitLine(input);
+        
+        // Assert
+        Assert.Equal(answer, splitLine);
+        
+    } 
 }
+
+
--- assignment-01/Assignment1/RegExpr.cs
+++ assignment-01/Assignment1/RegExpr.cs
@@ -1,10 +1,47 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment1;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    // public static void Main(string [] args)
+    // {
+    //     List<string> input = new List<string>(){"This", "is", "a", "test"};
+        
+    //     SplitLine(input);
+    //     Console.WriteLine("Done");
+    // }
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+       string pattern = @"\b\s+\b";
+       Regex rgx = new Regex(pattern);
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+       foreach(string wholeLine in lines)
+       {
+            foreach(string word in rgx.Split(wholeLine))
+            {
+                
+                yield return word;
+                
+            }
+       }   
+    }
+
+    public static IEnumerable<(int width, int height)> Resolutions(IEnumerable<string> resolutions)
+    {
+        string pattern = @"\b\s+\b";
+        Regex rgx = new Regex(pattern);
+        foreach(string wholeLine in lines)
+       {
+            foreach(string word in rgx.Split(wholeLine))
+            {
+                
+            }
+       }
+
+        return null;
+
+    } 
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
