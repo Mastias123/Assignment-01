036477f5e7e2a5aa821a91a13b7d6caad6bed1df
memr <memr@itu.dk>
2022-09-15 23:01:13 +0200

    fixInnterText

--- assignment-01/Assignment1/RegExpr.cs
+++ assignment-01/Assignment1/RegExpr.cs
@@ -4,13 +4,6 @@ namespace Assignment1;
 
 public static class RegExpr
 {
-    // public static void Main(string [] args)
-    // {
-    //     List<string> input = new List<string>(){"This", "is", "a", "test"};
-        
-    //     SplitLine(input);
-    //     Console.WriteLine("Done");
-    // }
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
     {
        string pattern = @"\b\s+\b";
@@ -19,10 +12,8 @@ public static class RegExpr
        foreach(string wholeLine in lines)
        {
             foreach(string word in rgx.Split(wholeLine))
-            {
-                
-                yield return word;
-                
+            {   
+                yield return word;                
             }
        }   
     }
@@ -30,18 +21,51 @@ public static class RegExpr
     public static IEnumerable<(int width, int height)> Resolutions(IEnumerable<string> resolutions)
     {
         string pattern = @"\b\s+\b";
+        string resolutionPattern = @"\D+";
+        
         Regex rgx = new Regex(pattern);
-        foreach(string wholeLine in lines)
+        Regex resolutionRgx = new Regex(resolutionPattern);
+
+        Dictionary<int, int> tempCollection = new Dictionary<int, int>();
+        List<int> keys = new List<int>();
+        List<int> values = new List<int>();
+
+
+        foreach(string wholeLine in resolutions)
        {
             foreach(string word in rgx.Split(wholeLine))
             {
-                
+                var result = resolutionRgx.Split(word);
+
+                for(int i = 0; i< result.Length ; i++)
+                {
+                    if(i%2 ==0)
+                    {
+                        int k = Int32.Parse(result[i]);
+                        keys.Add(k);
+                    }
+                    else
+                    {
+                        int v = Int32.Parse(result[i]);
+                        values.Add(v);
+                    }
+                }
             }
        }
-
-        return null;
-
+       for(int i = 0; i< keys.Count ; i++)
+       {
+            tempCollection.Add(keys[i], values[i]);
+            (int, int) tuple1 = (keys[i], values[i]);
+            yield return tuple1;
+       }
     } 
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    public static IEnumerable<string> InnerText(string html, string tag)
+    {
+        string pattern = $@"(<{tag}[\w\s\d_()\-:\/"":,.=]*>)(?<inner>.*?)(</{tag}>)";
+        foreach(Match match in Regex.Matches(html, pattern))
+        {
+            yield return match.Groups["inner"].Value;
+        }
+    } 
 }
